d280926a7c3730c4cf273c0dfbb6ded5ce6ddbc1
Janco Nel <neljanco92@Gmail.com>
2024-08-17 19:15:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 // LUMINIA V2 
 // This time were starting with it being open source
+
+const makeWASocket = require('@whiskeysockets/baileys').default;
+
+function connectionLogic() {
+  const sock = makeWAsocket({
+    printQRInTerminal: true,
+    )}
+    sock.ev.on('connection.update', async (update) => {
+
+      const {connection, lastDisconnect } = update;
+
+      if (update?.qr) {
+        console.log(qr);
+      }
+
+        if (connection == 'close') {
+            const shouldReconnect = 
+              lastDisconnect?.error?.output?.statusCode != 
+                DisconnectReason.loggedOut;
+            
+          
+        }
+    })
+}
